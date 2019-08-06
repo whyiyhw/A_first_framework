9b6d7afef1be01e366fc649f5f6fc5ca78da3be2
xuey <15827543830@163.com>
2019-08-06 17:33:03 +0800

    修改readme

--- README.md
+++ README.md
@@ -1,2 +1,10 @@
-# sakura
+# `Sakura`
 这是一个用来练习php框架组成的实验框架
+
+## 安装
+
+- 你可以使用 `composer create-project --prefer-dist whyiyhw/sakura blog`
+  - `cd blog php -S localhost:9090`
+  - 去本地浏览器输入 `localhost:9090`
+- 你也可以 `git clone https://github.com/whyiyhw/sakura.git` 来获取~  
+
--- app/Controllers/HomeController.php
+++ app/Controllers/HomeController.php
@@ -7,6 +7,9 @@ use App\Models\UserModel;
 use App\Services\RequestService;
 use App\Services\ResponseService;
 use App\Services\ViewServices;
+use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\HttpFoundation\Response;
+use Throwable;
 
 class HomeController
 {
@@ -14,13 +17,13 @@ class HomeController
     /**
      * 引入 了 Sy的组件 并对其 做出封装
      *
-     * @param RequestService $requestService
+     * @param RequestService $request
      * @param ResponseService $response
-     * @return \Symfony\Component\HttpFoundation\Response
+     * @return Response
      */
-    public function index(RequestService $requestService, ResponseService $response)
+    public function index(RequestService $request, ResponseService $response)
     {
-        $name = $requestService->get("name", "world");
+        $name = $request->get("name", "world");
 
         return $response->echo("hello {$name}");
     }
@@ -28,51 +31,47 @@ class HomeController
     /**
      * 返回第一个获取的 值
      *
-     * @param ResponseService $responseService
-     * @return \Symfony\Component\HttpFoundation\JsonResponse
+     * @param ResponseService $response
+     * @return JsonResponse
      */
-    public function user(ResponseService $responseService)
+    public function user(ResponseService $response)
     {
         $user = new UserModel();
         $res = $user->first();
-        return $responseService->json($res);
+        return $response->json($res);
     }
 
     /**
      * 使用 think-orm 进行数据库操作
      *
      * @param OrmModel $ormModel
-     * @param ResponseService $responseService
+     * @param ResponseService $response
      * @param int $id
-     * @return \Symfony\Component\HttpFoundation\JsonResponse
+     * @return JsonResponse
      */
-    public function ormUser(OrmModel $ormModel,ResponseService $responseService, $id = 2)
+    public function ormUser(OrmModel $ormModel, ResponseService $response, $id = 2)
     {
         try {
             $res = $ormModel->findOrFail(1);
             $res['default_id'] = $id;
-            return $responseService->json($res);
-        } catch (\Throwable $e) {
-            return $responseService->json([$e->getMessage()]);
+            return $response->json($res);
+        } catch (Throwable $e) {
+            return $response->json([$e->getMessage()]);
         }
     }
 
     /**
-     * 返回一个视图 或者 json 对象
-     *
-     * @param ResponseService $responseService
-     * @return \Symfony\Component\HttpFoundation\JsonResponse|\Symfony\Component\HttpFoundation\Response
+     * 返回一个视图
+     * @param ResponseService $response
+     * @return Response
+     * @throws Throwable
      */
-    public function viewUser(ResponseService $responseService)
+    public function viewUser(ResponseService $response)
     {
         $user = new OrmModel();
-        try {
-            $res = $user->findOrFail(1);
-            $view = new ViewServices();
-            $view->display("index", ['name' => $res['name']]);
-            return $responseService->echo();
-        } catch (\Throwable $e) {
-            return $responseService->json([$e->getMessage()]);
-        }
+        $res = $user->findOrFail(1);
+        $view = new ViewServices();
+        $view->display("index", ['name' => $res['name']]);
+        return $response->echo();
     }
 }
